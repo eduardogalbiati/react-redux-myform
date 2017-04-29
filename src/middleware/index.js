d825b706ee90e20a546c8f32af87cccb50ab8833
Eduardo Galbiati <eduardo.galbiati7@gmail.com>
2017-04-29 17:01:47 -0300

    Cleaning repo

--- src/middleware/index.js
+++
@@ -1,9 +0,0 @@
-import { UPDATE_LOCATION } from 'react-router-redux'
-
-// hook into any action without to use the reducer
-export default store => next => action => {
-  if (action.type === UPDATE_LOCATION) {
-    // console.log('page changed')
-  }
-  return next(action)
-}
