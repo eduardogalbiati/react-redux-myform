ab43c4296ab67f9629b028ce2a2295eb5e1238b9
Eduardo Galbiati <eduardo.galbiati7@gmail.com>
2017-04-29 17:02:45 -0300

    NPM Module

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import MyForm from './node_modules/components/MyForm'
+
+class App extends Component {
+  render() {
+    return (
+      <MyForm />
+    );
+  }
+}
+
+export default App;
